5e2c717bd585acc3de668cc0d24c664359ffd6e0
gjmreborn2 <grzegorzmalyska720@gmail.com>
2024-11-17 22:44:22 +0100

    temp commit

--- init/init.c
+++ init/init.c
@@ -8,10 +8,17 @@
 
 void display_boot_info(struct boot_info *binfo);
 
-void kmain(uint32_t magic, uint32_t boot_info_addr) {
+/* from linker script */
+extern uint32_t kernel_start;
+extern uint32_t kernel_end;
+
+extern void reprogram_pic();
+
+void kmain(uint16_t *root_dev, uint16_t *swap_dev, uint32_t magic, uint32_t boot_info_addr) {
     struct boot_info binfo;
 
 	kill_floppy_motor();
+    reprogram_pic();
 
     console_init();
     console_cls();
@@ -25,7 +32,16 @@ void kmain(uint32_t magic, uint32_t boot_info_addr) {
         return;
     }
 
+    /*
+        add current cursor position, hdd data (cylinders, heads, start cylinder, pre-compensation cylinder, maximum ECC burst size, control byte, standard timeout value, format timeout value, detect drive timeout value, head landing (stop) cylinder number, number of sectors per track)
+        , nr of screen columns, display mode, current display page
+    */
     display_boot_info(&binfo);
+    uint16_t ROOT_DEV = *root_dev;
+    uint16_t SWAP_DEV = *swap_dev;
+    kprintf("root dev = 0x%x, swap dev = 0x%x\n", ROOT_DEV, SWAP_DEV);
+    kprintf("kernel base address: = 0x%x, kernel end address = 0x%x\n", &kernel_start, &kernel_end);
+    kprintf("kernel size: %d bytes\n", &kernel_end - &kernel_start);
 
     while(1) {}
 }
